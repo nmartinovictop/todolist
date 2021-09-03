92d0b5ec36402038bced8210fbe641014f6c1461
Nick Martinovic TOP <nmartinovictop@gmail.com>
2021-09-04 08:11:45 +0900

    for repl

--- main.js
+++ main.js
@@ -5,19 +5,13 @@ var __webpack_exports__ = {};
   \**********************/
 console.log('webpack is working')
 
-// import {toDoFactory} from './todoObject.js'
-
-let nick = 'nicktext'
-
-
 const toDoFactory = (title, description, dueDate, priority, project, isCompleted) => {
-
-
     return { title, description, dueDate, priority, project, isCompleted}
 }
 
-console.log('test2')
-// const todo1 = toDoFactory('nick','nick\'s description', '2021-09-04',1,'inbox',false)
+const nick = 'nick'
+
+
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBLFdBQVcsYUFBYTs7QUFFeEI7OztBQUdBOzs7QUFHQSxhQUFhO0FBQ2I7O0FBRUE7QUFDQSx3RiIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG9saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd3ZWJwYWNrIGlzIHdvcmtpbmcnKVxuXG4vLyBpbXBvcnQge3RvRG9GYWN0b3J5fSBmcm9tICcuL3RvZG9PYmplY3QuanMnXG5cbmxldCBuaWNrID0gJ25pY2t0ZXh0J1xuXG5cbmNvbnN0IHRvRG9GYWN0b3J5ID0gKHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIHByb2plY3QsIGlzQ29tcGxldGVkKSA9PiB7XG5cblxuICAgIHJldHVybiB7IHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIHByb2plY3QsIGlzQ29tcGxldGVkfVxufVxuXG5jb25zb2xlLmxvZygndGVzdDInKVxuLy8gY29uc3QgdG9kbzEgPSB0b0RvRmFjdG9yeSgnbmljaycsJ25pY2tcXCdzIGRlc2NyaXB0aW9uJywgJzIwMjEtMDktMDQnLDEsJ2luYm94JyxmYWxzZSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0EsYUFBYTtBQUNiOztBQUVBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3dlYnBhY2sgaXMgd29ya2luZycpXG5cbmNvbnN0IHRvRG9GYWN0b3J5ID0gKHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIHByb2plY3QsIGlzQ29tcGxldGVkKSA9PiB7XG4gICAgcmV0dXJuIHsgdGl0bGUsIGRlc2NyaXB0aW9uLCBkdWVEYXRlLCBwcmlvcml0eSwgcHJvamVjdCwgaXNDb21wbGV0ZWR9XG59XG5cbmNvbnN0IG5pY2sgPSAnbmljaydcblxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
