447e9ed7d62d24f5471c39241c02274c05a3b775
Nick Martinovic TOP <nmartinovictop@gmail.com>
2021-09-04 08:06:44 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('webpack is working')
+
+// import {toDoFactory} from './todoObject.js'
+
+let nick = 'nicktext'
+
+
+const toDoFactory = (title, description, dueDate, priority, project, isCompleted) => {
+
+
+    return { title, description, dueDate, priority, project, isCompleted}
+}
+
+console.log('test2')
+// const todo1 = toDoFactory('nick','nick\'s description', '2021-09-04',1,'inbox',false)
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBLFdBQVcsYUFBYTs7QUFFeEI7OztBQUdBOzs7QUFHQSxhQUFhO0FBQ2I7O0FBRUE7QUFDQSx3RiIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG9saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd3ZWJwYWNrIGlzIHdvcmtpbmcnKVxuXG4vLyBpbXBvcnQge3RvRG9GYWN0b3J5fSBmcm9tICcuL3RvZG9PYmplY3QuanMnXG5cbmxldCBuaWNrID0gJ25pY2t0ZXh0J1xuXG5cbmNvbnN0IHRvRG9GYWN0b3J5ID0gKHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIHByb2plY3QsIGlzQ29tcGxldGVkKSA9PiB7XG5cblxuICAgIHJldHVybiB7IHRpdGxlLCBkZXNjcmlwdGlvbiwgZHVlRGF0ZSwgcHJpb3JpdHksIHByb2plY3QsIGlzQ29tcGxldGVkfVxufVxuXG5jb25zb2xlLmxvZygndGVzdDInKVxuLy8gY29uc3QgdG9kbzEgPSB0b0RvRmFjdG9yeSgnbmljaycsJ25pY2tcXCdzIGRlc2NyaXB0aW9uJywgJzIwMjEtMDktMDQnLDEsJ2luYm94JyxmYWxzZSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
